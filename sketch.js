edc87d7969c396f8872487d5e3b709d374079b10
freirecaio-rgb <freire.caio@escola.pr.gov.br>
2026-08-31 21:58:10 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,34 @@
+let tamanho = 80;
+
+function setup() {
+    let canvas = createCanvas(400, 200);
+
+    canvas.parent("p5-container");
+}
+
+function draw() {
+    // Fundo
+    background(230);
+
+    // Texto
+    textAlign(CENTER);
+    textSize(20);
+    fill(40);
+
+    text("Clique no quadro!", width / 2, 30);
+
+    // Círculo que acompanha o mouse
+    fill(80);
+    circle(mouseX, mouseY, tamanho);
+}
+
+function mousePressed() {
+    // Aumenta o tamanho do círculo quando clicar
+    tamanho += 20;
+
+    // Volta ao tamanho inicial
+    if (tamanho > 180) {
+        tamanho = 80;
+    }
+}
 
