2074333f4224b8e1ddeb385f7a91e0cbdcb3d0e0
freirecaio-rgb <freire.caio@escola.pr.gov.br>
2026-08-31 22:09:11 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,26 +2,26 @@
 let tamanho = 80;
 
 function setup() {
-    // Cria o quadro do p5.js
-    let canvas = createCanvas(400, 200);
+    // Cria o quadro
+    let canvas = createCanvas(500, 250);
 
     // Coloca o quadro dentro da div
     canvas.parent("p5-container");
 }
 
 function draw() {
-    // Fundo do quadro
-    background(230);
+    // Fundo
+    background(245);
 
     // Texto
     textAlign(CENTER);
     textSize(20);
-    fill(40);
+    fill(50);
 
     text("Clique no círculo!", width / 2, 30);
 
     // Círculo seguindo o mouse
-    fill(80);
+    fill(100);
     circle(mouseX, mouseY, tamanho);
 }
 
